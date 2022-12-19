5b2c1cf3a750611e2645b9d650ab8ba4e775c6cb
matanLevi404 <matan.levi.404@gmail.com>
2022-12-19 19:49:48 +0200

    checking session

--- helpers/onlyLoggedUsers.js
+++ helpers/onlyLoggedUsers.js
@@ -1,6 +1,5 @@
 const onlyLoggedUsers = (req, res, next) => {
-  console.log(req.session.Session, "from middleware");
-  if (req.session.user_id && req.session.user_id != 1) {
+  if (req.session) {
     next();
   } else {
     res.status(401).send({ err: "sensetive content for logged users only" });
